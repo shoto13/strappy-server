9333a7a6b3986ce90786ec72b4096dcf440e3c6e
shoto13 <wilsonrc@pm.me>
2025-01-07 10:29:59 +0000

    uri update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 app.use(cors());
 
 //TO run locally re-create .env file locally
-mongoose.connect(process.env.MONGO-URI, {useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
+mongoose.connect(process.env.MONGOURI, {useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log("Error connecting to database",err);
