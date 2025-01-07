f9ef795e9eedae97802b8eb6f4f2823010aa465f
shoto13 <wilsonrc@pm.me>
2025-01-07 10:13:30 +0000

    package additions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-mongoose.connect(process.env.MONGO-URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
+mongoose.connect(process.env.MONGO-URI, {useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log("Error connecting to database",err);
