4878ea9b21bcaceb4da3c1c789e5830740c606bc
shoto13 <58400642+shoto13@users.noreply.github.com>
2025-04-11 14:01:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,53 +4,55 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 require('dotenv').config();
 
-
 const app = express();
 const cors = require('cors');
 app.use(cors());
 
-//TO run locally re-create .env file locally
-mongoose.connect(process.env.MONGOURI, {useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
+// Connect to MongoDB
+mongoose.connect(process.env.MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.log("Error connecting to database",err);
-});
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running on assigned PORT");
+  console.log("Server running on assigned PORT");
 });
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello world</h1>')
-})
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
-
-//models required to format data
+// Models
 const Watch = require("./models/watch");
-const Strap = require("./models/strap")
+const Strap = require("./models/strap");
 
+// Get specific watch by full reference
+app.get("/watches/:reference", (req, res) => {
+  const inputref = req.params.reference.toUpperCase();
 
-
-app.get("/watches/:reference", (req,res) => {
-    const inputref = req.params.reference
-
-    Watch.findOne({reference : {$eq : inputref}})
-        .then((watch) => {
-            if(watch == null){
-                //watch could not be found
-                console.log('watch could not be located')
-                return res.status(404).json({message: "Watch not located"})
-            } else {
-                console.log('watch was received');
-                console.log(watch);
-                res.status(200).json(watch);
-            }
-        })
-        .catch((err) => {
-            console.log("Error retrieving watch", err);
-            res.status(500).json({message: "error retrieving watches"})
-        })
+  Watch.findOne({ reference: { $eq: inputref } })
+    .then((watch) => {
+      if (!watch) {
+        console.log('watch could not be located');
+        return res.status(404).json({ message: "Watch not located" });
+      } else {
+        console.log('watch was received');
+        res.status(200).json(watch);
+      }
+    })
+    .catch((err) => {
+      console.log("Error retrieving watch", err);
+      res.status(500).json({ message: "error retrieving watches" });
+    });
 });
+
+// Get similar references based on logic (dash or dot = full reference)
 app.get("/watches/similar/:baseReference", async (req, res) => {
   const baseRef = req.params.baseReference.toUpperCase();
   const limit = parseInt(req.query.limit) || 10;
@@ -58,14 +60,11 @@ app.get("/watches/similar/:baseReference", async (req, res) => {
 
   try {
     let query;
-
     const isFullRef = baseRef.includes("-") || baseRef.includes(".");
 
     if (isFullRef) {
-      // It's a full reference → return only exact match
       query = { reference: baseRef };
     } else {
-      // It's a base model → return refs starting with baseRef + dash or dot
       query = {
         reference: {
           $regex: `^${baseRef}[-.]`,
@@ -82,11 +81,10 @@ app.get("/watches/similar/:baseReference", async (req, res) => {
   } catch (err) {
     console.error("Error retrieving similar references:", err);
     res.status(500).json({ message: "Error fetching similar references" });
+  }
+});
 
-
-
-
-
+// Get watches by prefix (shared base like 116200)
 app.get("/watches/prefix/:make/:prefix", (req, res) => {
   const make = req.params.make.toLowerCase();
   const prefix = req.params.prefix;
@@ -108,56 +106,38 @@ app.get("/watches/prefix/:make/:prefix", (req, res) => {
     });
 });
 
-
-
-
-//endpoint to get all the straps associated with a watch by id
+// Get straps for a given reference or lug_width
 app.get("/straps/:reference", (req, res) => {
-    console.log("here should be the vals" ,req.params);
+  console.log("here should be the vals", req.params);
+  const straps = req.params;
+  let vals = straps.reference;
+  let newArray = vals.split(",");
 
-    //FUDGE TO TAKE THE REQ PARAMS AND TURN INTO ARRAY IN A HACKY WAY
-    // NEEDS BETTER FIX
-    const straps = req.params;
-    let vals = straps.reference;
-    let newArray = vals.split(",");
-
-    // CONDITIONAL REQUEST BASED ON LENGTH OF INPUT VALUE
-    //TODO SEE IF WE CAN DO THIS MORE ELEGANTLY...
-    //IF THE VALUE IS GREATER IN LENGTH THAN 2, WE MUST BE SEARCHING FOR STRAP REFERENCES
-
-    //IF THE VALUE IS 2 DIGITS OR LESS WE MUST BE SEARCHING FOR LUG WIDTH VALUES 
-
-
-    if (newArray[0].length > 3) {
-        Strap.find({ reference: { $in: newArray }})
-        .then((strap) => {
-  
-          //strap.forEach(printjson);
-          res.status(200).json(strap);
-        })
-        .catch((err) => {
-          console.log("Error retrieving straps", err);
-          res.status(500).json({ message: "Error retrieving straps" });
-        });
-    } else {
-        Strap.find({ lug_width: { $in: newArray }})
-        .then((strap) => {
-        //   console.log(strap);
-          res.status(200).json(strap);
-          console.log("Relevant straps were located correctly");
-        })
-        .catch((err) => {
-          console.log("Error retrieving straps", err);
-          res.status(500).json({ message: "Error retrieving straps" });
-        });
-    }
-
-  });
-
-    //endpoint to allow us to retrieve all straps with a matching lug width
-    app.get("/straps/:lug_width", (req, res) => {
-        console.log("lug widths: ",req.params);
-        const lug_width = req.params;
-    
+  if (newArray[0].length > 3) {
+    Strap.find({ reference: { $in: newArray } })
+      .then((strap) => {
+        res.status(200).json(strap);
+      })
+      .catch((err) => {
+        console.log("Error retrieving straps", err);
+        res.status(500).json({ message: "Error retrieving straps" });
       });
-    
+  } else {
+    Strap.find({ lug_width: { $in: newArray } })
+      .then((strap) => {
+        res.status(200).json(strap);
+        console.log("Relevant straps were located correctly");
+      })
+      .catch((err) => {
+        console.log("Error retrieving straps", err);
+        res.status(500).json({ message: "Error retrieving straps" });
+      });
+  }
+});
+
+// Placeholder endpoint for future use
+app.get("/straps/:lug_width", (req, res) => {
+  console.log("lug widths: ", req.params);
+  const lug_width = req.params;
+});
+
